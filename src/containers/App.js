231a22abec3ff3bf34a36dba42f23f9623e86a9c
Brian Sserumaga <briansserumaga@Your-Girlfriends-bestfriend.local>
2018-01-29 16:09:19 +0300

    Added the different routes.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
-import Header from "../components/header";
+import Header from "../components/common/header";
+import { Headliners } from "../containers/headliners";
+import { General } from "../containers/general";
+import { Sources } from "../containers/sources";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        
+        <BrowserRouter >
+          <Switch>
+            <Route path="/sources" render ={props => <Sources {...props} />} ></Route>
+            <Route path="/general" render ={props => <General {...props} />} ></Route>
+            <Route path="/" render ={props => <Headliners {...props} />} ></Route>
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
