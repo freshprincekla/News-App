d2e01ea56b17ef52cd0d7ff29954d6d14da3fa2d
Brian Sserumaga <briansserumaga@Your-Girlfriends-bestfriend.local>
2018-01-29 16:07:33 +0300

    Moved the head component to the common folder.

---
+++ src/components/common/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import Link from "react-dom";
+
+const Header = () => {
+    return(
+        <div>
+            <ul>
+                <li>Top Headlines </li>
+                <li>News</li>
+                <li>Sources</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header;
